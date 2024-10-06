6416ff775c83b656e4ba1f6d44a4a9f4c3579bb9
Ethan <175902687+kokorocks@users.noreply.github.com>
2024-10-05 20:29:37 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 // Define a function to fetch and execute tasks from the server
-async function fetchTask() {
+/*async function fetchTask() {
     try {
         const response = await fetch('/get-task'); // Endpoint to get a task
         if (!response.ok) throw new Error('Network response was not ok');
@@ -49,3 +49,41 @@ function processTask(task) {
 
 // Start processing tasks
 main();
+*/
+
+const peer = new Peer();
+let conn;
+
+peer.on('open', (id) => {
+    console.log('My peer ID is: ' + id);
+    alert('Your peer ID is: ' + id);
+});
+
+peer.on('connection', (connection) => {
+    conn = connection;
+    conn.on('data', (data) => {
+        document.getElementById('output').innerText = data;
+    });
+});
+
+function connectPeer() {
+    const peerId = prompt('Enter peer ID:');
+    conn = peer.connect(peerId);
+    conn.on('open', () => {
+        console.log('Connected to: ' + peerId);
+    });
+    conn.on('data', (data) => {
+        alert('congrats, you just got '+data+'$')
+        document.cookie=document.cookie+data
+    });
+}
+
+function sendData() {
+    const data = document.getElementById('dataInput').value;
+    if(!data > document.cookie){
+    if (conn && conn.open) {
+        conn.send(data);
+    } else {
+        alert('Not connected to any peer');
+    }
+}}else{alert('you do not enough money')}
